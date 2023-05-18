be7648492848b3528429f66ec1959dcd900d9f56
Sergio <sergio.denn.navarro@gmail.com>
2023-05-18 12:52:59 -0400

    Adding fixes (SonarQube)

--- app/src/main/java/katas/persona/PersonaFilters.java
+++ app/src/main/java/katas/persona/PersonaFilters.java
@@ -4,13 +4,12 @@
 
 package katas.persona;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PersonaFilters {
   public List<Persona> ageFilterOlder(List<Persona> lista, int age) {
-    List<Persona> res = new ArrayList<>();
+    List<Persona> res;
     res = lista.stream()
         .filter(p -> p.getAge() >= age)
         .collect(Collectors.toList());
@@ -18,7 +17,7 @@ public class PersonaFilters {
   }
 
   public List<Persona> ageFilterYoung(List<Persona> lista, int age) {
-    List<Persona> res = new ArrayList<>();
+    List<Persona> res;
     res = lista.stream()
         .filter(p -> p.getAge() <= age)
         .collect(Collectors.toList());
